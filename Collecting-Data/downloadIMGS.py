ec43a1c69747160cb42bbfc64f47d1c4f703449b
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-03-28 12:33:30 +0300

    download imgs updated

--- Collecting-Data/downloadIMGS.py
+++ Collecting-Data/downloadIMGS.py
@@ -3,57 +3,51 @@ import requests
 import os 
 
 def get_data():
-    df_list = []
-
     data_path = '../Data/news-data.csv'
-
     df = pd.read_csv(data_path)
-    df = df.iloc[:100]
+    df = df.iloc[:1000]
     return df
 
 def download_img(url, content_url):
     folder = '../Data/imgs' 
-    try:    
+    try:
         response = requests.get(url)
         if response.status_code == 200:
-            
             img_type = url.split('.')[-1]
-            
             content_url = content_url.strip()
-            
-            img_path = f'{folder}/{content_url}.{img_type}' 
+            img_name = content_url.replace('/', '_') + '.' + img_type
+
+            img_path = os.path.join(folder, img_name)
+
+            if os.path.exists(img_path):
+                return img_path
 
             with open(img_path, 'wb') as f:
                 f.write(response.content)
 
             return img_path
         else:
-            return None
+            print(f"Failed to download image from {url}. Status code: {response.status_code}")
+            return Nonex
 
     except Exception as e:
-        print(f"Img could not be downloaded,{content_url}.")
+        print(f"Error downloading image from {url}: {e}")
         return None
 
 def main():
     df = get_data()
 
-    df['img_path'] = None
-
     folder = '../Data/imgs' 
 
-    if os.path.exists(folder):
-        os.rmdir(path=folder)
-
-    os.makedirs(folder)
+    os.makedirs(folder, exist_ok=True)
      
     for i, img_url in enumerate(df['Img_url']):
-
         try:
             img = img_url.split('?u=')[1]
-        except:
+        except IndexError:
             img = img_url.split('?u=')[0]
         
-        content_url = df[df['Img_url'] == img_url]['Content_url'].values[0]
+        content_url = df.at[i, 'Content_url']
         content_url = content_url.replace('/', '_')
 
         img_path = download_img(img, content_url)
